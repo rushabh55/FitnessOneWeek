4661c63ab3398d74f9a8c0f1800bd471951353cf
Rushabh Gosar <rugos@microsoft.com>
2017-07-22 16:26:12 -0700

    Adding realm db support

---
+++ FitnessProjectOneWeek/FitnessProjectOneWeek/UX/Start/StartExperience.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  FitnessProjectOneWeek
+//
+//  Created by redmond\rugos on 7/19/17.
+//  Copyright © 2017 Rushabh Gosar. All rights reserved.
+//
+@import Foundation;
+#import "StartExperience.h"
+#import <SDWebImage/SDWebImageManager.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <AFNetworking.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginButton.h>
+
+@interface StartExperience ()
+
+@end
+
+@implementation StartExperience
+
+- (void)viewDidLoad
+{
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends"];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
+    
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSMutableDictionary* parameters = [NSMutableDictionary dictionary];
+    [parameters setValue:@"id,name,email" forKey:@"fields"];
+    
+    [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:parameters]
+     startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection,
+                                  id result, NSError *error)
+     {
+         NSDictionary *dictionary = (NSDictionary *)result;
+         NSString *email = [dictionary objectForKey:@"email"];
+         if (email.length)
+         {
+             
+         }
+     }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
